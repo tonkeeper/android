961dcaff0f064edd47694926822aa13f7a526245
polstianka <peer_id@yahoo.com>
2024-01-19 11:20:32 -0800

    remove build folder for signer app

--- apps/singer/build/tmp/kapt3/stubs/debug/com/tonapps/singer/core/Vault.java
+++
@@ -1,9 +0,0 @@
-package com.tonapps.singer.core;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/tonapps/singer/core/Vault;", "", "()V", "singer_debug"})
-public final class Vault {
-    
-    public Vault() {
-        super();
-    }
-}

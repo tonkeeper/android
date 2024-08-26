bbad74f2012f278519ac5a412b3916abad65670f
polstianka <peer_id@yahoo.com>
2024-08-26 06:52:12 -0400

    up version

--- buildSrc/src/main/kotlin/Build.kt
+++ buildSrc/src/main/kotlin/Build.kt
@@ -1,6 +1,6 @@
 object Build {
     const val compileSdkVersion = 34
-    const val minSdkVersion = 24
+    const val minSdkVersion = 26
 
     fun namespacePrefix(name: String) = "com.tonapps.$name"
 }

9cf33bbb7695f72308dcc2d87eca6864b3274806
polstianka <peer_id@yahoo.com>
2024-09-06 09:57:57 -0400

    from dev

--- apps/wallet/instance/app/src/main/java/com/tonapps/tonkeeper/ui/screen/send/main/SendException.kt
+++ apps/wallet/instance/app/src/main/java/com/tonapps/tonkeeper/ui/screen/send/main/SendException.kt
@@ -1,6 +1,7 @@
 package com.tonapps.tonkeeper.ui.screen.send.main
 
 sealed class SendException: Exception() {
+    class FailedToSendTransaction: SendException()
     class UnableSendTransaction: SendException()
     class WrongPasscode: SendException()
     class Cancelled: SendException()
